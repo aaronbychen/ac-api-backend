f620ae230a8db0815430fc6a078e324db090541b
Aaron Chen <aaronbychen@gmail.com>
2024-08-20 23:20:44 +0800

    Update UserMapper.java

--- ac-api-backend/src/main/java/com/ac/springbootinit/mapper/UserMapper.java
+++ ac-api-backend/src/main/java/com/ac/springbootinit/mapper/UserMapper.java
@@ -6,8 +6,7 @@ import com.ac.springbootinit.model.entity.User;
 /**
  * 用户数据库操作
  *
- * @author <a href="https://github.com/liac">程序员鱼皮</a>
- * @from <a href="https://ac.icu">编程导航知识星球</a>
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 public interface UserMapper extends BaseMapper<User> {
 
