d8ae4a96778712c27fb6f278cc1ec246fb4a9fc8
Aaron Chen <aaronbychen@gmail.com>
2024-08-21 21:50:16 +0800

    Update UserServiceTest.java

--- ac-api-backend/src/test/java/com/ac/springbootinit/service/UserServiceTest.java
+++ ac-api-backend/src/test/java/com/ac/springbootinit/service/UserServiceTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 /**
  * 用户服务测试
  *
- * @author <a href="https://github.com/liac">程序员鱼皮</a>
- * @from <a href="https://ac.icu">编程导航知识星球</a>
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 @SpringBootTest
 public class UserServiceTest {
