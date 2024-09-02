9c5755cb91ec6ee43e9576adbc6c37737f630a58
Aaron Chen <aaronbychen@gmail.com>
2024-09-01 23:27:49 -0500

    Update BaseResponse.java

--- ac-api-backend/src/main/java/com/ac/springbootinit/common/BaseResponse.java
+++ ac-api-backend/src/main/java/com/ac/springbootinit/common/BaseResponse.java
@@ -7,8 +7,7 @@ import lombok.Data;
  * 通用返回类
  *
  * @param <T>
- * @author <a href="https://github.com/liac">程序员鱼皮</a>
- * @from <a href="https://ac.icu">编程导航知识星球</a>
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 @Data
 public class BaseResponse<T> implements Serializable {
