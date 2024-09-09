9a67ada42bf271c7c51e0c6836fe629c95641d67
Aaron Chen <aaronbychen@gmail.com>
2024-09-08 23:58:09 -0500

    Update MyBatisPlusConfig.java

--- ac-api-backend/src/main/java/com/ac/springbootinit/config/MyBatisPlusConfig.java
+++ ac-api-backend/src/main/java/com/ac/springbootinit/config/MyBatisPlusConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * MyBatis Plus 配置
  *
- * @author https://github.com/liac
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 @Configuration
 @MapperScan("com.ac.springbootinit.mapper")
